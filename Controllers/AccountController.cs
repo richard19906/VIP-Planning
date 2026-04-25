839820d3a03c3e3abc477de9b8efda7c88a282eb
Richard <richard19906@gmail.com>
2026-04-25 05:11:02 +0200

    Emergency Fix: 500 error solved, Removed Uren/Personnel, New Master Layout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,34 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using System.Linq;
+using System;
 
 namespace VIP_Planning.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() => View();
+        public IActionResult Planning() => View();
+        public IActionResult Instellingen() => View();
+
+        [HttpPost]
+        public IActionResult SavePincode(string newPin) {
+            return RedirectToAction("Instellingen");
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePassword(string oldPw, string newPw) {
+            return RedirectToAction("Instellingen");
+        }
+    }
+
     public class AccountController : Controller {
         [HttpGet] public IActionResult Login() => View();
         [HttpGet] public IActionResult Register() => View();
         [HttpGet] public IActionResult VerifyCode() => View();
 
         [HttpPost]
-        public IActionResult Register(string naam, string email, string password, string confirmPassword) {
-            if (password != confirmPassword) {
-                ViewBag.Error = "Wachtwoorden komen niet overeen.";
-                return View();
-            }
-            TempData["UserEmail"] = email;
-            return RedirectToAction("VerifyCode");
-        }
-
-        [HttpPost]
         public IActionResult Verify(string username, string pincode) {
-            string[] adminCodes = { "3991", "0000", "1234", "admin" };
-            if (adminCodes.Contains(pincode)) {
+            if (pincode == "3991" || pincode == "0000") {
                 HttpContext.Session.SetString("IsLoggedIn", "true");
                 return RedirectToAction("Index", "Home");
             }
-            ViewBag.Error = "Toegang geweigerd.";
             return View("Login");
         }
-
         public IActionResult Logout() {
             HttpContext.Session.Clear();
             return RedirectToAction("Login");
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
+using System;
 
 namespace VIP_Planning.Controllers {
     public class HomeController : Controller {
         public IActionResult Index() => View();
         public IActionResult Planning() => View();
-        public IActionResult Werknemers() => View();
         public IActionResult Instellingen() => View();
 
         [HttpPost]
         public IActionResult SavePincode(string newPin) {
-            // Hier komt database save actie
             return RedirectToAction("Instellingen");
         }
 
         [HttpPost]
         public IActionResult UpdatePassword(string oldPw, string newPw) {
-            // Hier komt password update actie via Supabase
             return RedirectToAction("Instellingen");
+        }
+    }
+
+    public class AccountController : Controller {
+        [HttpGet] public IActionResult Login() => View();
+        [HttpGet] public IActionResult Register() => View();
+        [HttpGet] public IActionResult VerifyCode() => View();
+
+        [HttpPost]
+        public IActionResult Verify(string username, string pincode) {
+            if (pincode == "3991" || pincode == "0000") {
+                HttpContext.Session.SetString("IsLoggedIn", "true");
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Login");
+        }
+        public IActionResult Logout() {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
         }
     }
 }
