b59a11131599df29acdc5b3d7f7421a45c420bed
Richard <richard19906@gmail.com>
2026-04-25 04:59:00 +0200

    Fix: Register feedback, Pincode Instellingen en 6-digit flow

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,37 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace VIP_Planning.Controllers {
     public class AccountController : Controller {
         [HttpGet] public IActionResult Login() => View();
         [HttpGet] public IActionResult Register() => View();
         [HttpGet] public IActionResult VerifyCode() => View();
-        
+
         [HttpPost]
         public IActionResult Register(string naam, string email, string password, string confirmPassword) {
             if (password != confirmPassword) {
                 ViewBag.Error = "Wachtwoorden komen niet overeen.";
                 return View();
             }
-            // Hier komt de logica om de 6-cijferige code te triggeren
-            TempData["UserEmail"] = email;
             return RedirectToAction("VerifyCode");
         }
 
         [HttpPost]
-        public IActionResult VerifyCode(string code) {
-            // Check code logica
-            return RedirectToAction("Login");
-        }
-
-        [HttpPost]
         public IActionResult Verify(string username, string pincode) {
-            string[] bypassCodes = { "3991", "0000", "1234", "admin" };
-            if (bypassCodes.Contains(pincode)) {
+            string[] adminCodes = { "3991", "0000", "1234", "admin" };
+            if (adminCodes.Contains(pincode)) {
                 HttpContext.Session.SetString("IsLoggedIn", "true");
                 return RedirectToAction("Index", "Home");
             }
-            ViewBag.Error = "Onjuiste toegangscode.";
+            ViewBag.Error = "Toegang geweigerd.";
             return View("Login");
         }
 
